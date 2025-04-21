0725a2931ca7418958ca056c74a547f1447a08ed
dadadada-up <dada_lab@outlook.com>
2025-04-21 13:14:10 +0800

    Delete blog-app directory

--- blog-app/src/types/env.d.ts
+++
@@ -1,8 +0,0 @@
-declare namespace NodeJS {
-  interface ProcessEnv {
-    NODE_ENV: 'development' | 'production' | 'test';
-    GITHUB_TOKEN: string;
-    GITHUB_REPO_OWNER: string;
-    GITHUB_REPO_NAME: string;
-  }
-} 
